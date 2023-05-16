756dd716c07e56d430bbc541fcf69ae237703740
YunJi <kwg531@naver.com>
2023-05-17 01:49:32 +0900

    FEAT: Implementing list import

--- src/main/java/com/example/myblog/controller/ArticleController.java
+++ src/main/java/com/example/myblog/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.example.myblog.controller;
 import com.example.myblog.dto.ArticleForm;
 import com.example.myblog.entity.Article;
 import com.example.myblog.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j // 로깅 (logging) 기능 추가, Lombok 플러그인 설치 필요
+@RequiredArgsConstructor // final 필드 값을 알아서 가져옴 (@autowired 대체)
 @Controller // 컨트롤러 선언, 요청과 응답 처리
 public class ArticleController {
 
@@ -19,14 +21,18 @@ public class ArticleController {
     private ArticleRepository articleRepository;
 
     @GetMapping("/articles") // 해당 요청을 처리하도록, 메소드를 등록
-    public String index(Model model) {  // 모델 객체를 가져옴
+    public String index(Model model) {  // 뷰 페이지로 데이터 전달을 위한 Model 객체 자동 삽입 됨
        /*
-            데이터를 모델에 등록, 등록된 데이터는 뷰 페이지에서 사용 가능
-            key (변수명): "msg",
-            value (내용): "안녕하세요, 반갑습니다!"
+            모든 Article 을 가져옴
+            Iterable 인터페이스는 ArrayList의 부모 인터페이스
         */
-        model.addAttribute("msg", "안녕하세요, 반갑습니다!");
-        return "articles/index"; // 뷰 페이지 설정
+        Iterable<Article> articleList = articleRepository.findAll();
+
+        // 뷰 페이지로 article 전달
+        model.addAttribute("articles", articleList);
+
+        // 뷰 페이지 설정
+        return "articles/index";
     }
     
     @GetMapping("/articles/new") // GET 요청이 해당 url로 오면, 아래 메소드를 수행
